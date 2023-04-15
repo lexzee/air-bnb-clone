07c1e65d2cf414729f4a5ddf0120bdef8bf0da98
Abdulquddus Abdulrahman (Lexzee) <developerlexzee@gmail.com>
2023-04-15 22:30:16 +0100

    initialized react with vite

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import logo from '../assets/images/airbnb-logo.png'
+
+const Navbar = () => {
+  return (
+    <nav className="nav">
+      <img src={logo} />
+    </nav>
+  )
+}
+
+export default Navbar
