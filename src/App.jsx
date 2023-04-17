f52cf6165c6c3e6fc0f0be522a38b8adff27b588
Abdulquddus Abdulrahman (Lexzee) <developerlexzee@gmail.com>
2023-04-17 05:28:08 +0100

    added a Card component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,7 +12,11 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <Hero />
+      {/* <Hero /> */}
+      <div className='cards'>
+        <Card />
+        {/* <Card/> */}
+      </div>
     </div>
   )
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,29 @@
+import pic1 from '../assets/images/katie-zaferes.png'
+import star from '../assets/images/star.png'
+
+const Card = () => {
+  return (
+    <section className='card'>
+      <div className='img'>
+        {/* <div className='img--overlay'> */}
+          <img src={pic1} alt='Katie Zaferes' className='card-img'/>
+          <div>sold out</div>
+        {/* </div> */}
+      </div>
+      <div className='rating'>
+        <img src={star} />
+        <p>5.0
+        <span>(6) * USA</span>
+        </p>
+      </div>
+      <p className='title'>
+        Life lessons with Katie Zafares
+      </p>
+      <p className='cost-person'>
+        <span>From $136</span> / person
+      </p>
+    </section>
+  )
+}
+
+export default Card
