d5174d3aeec49b7c6f5dbeae2c0e7cbb793830d5
Abdulquddus Abdulrahman (Lexzee) <developerlexzee@gmail.com>
2023-04-19 17:21:54 +0100

    added reusability to the card components using props

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,14 +7,36 @@ import Hero from './components/Hero'
 import Card from './components/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+  const data = [
+    {
+      img:"katie-zaferes.png",
+      rating:"5.0",
+      reviewCount:6,
+      country:"USA",
+      title:"Life lessons with Katie Zafares",
+      price:136
+    }
+  ]
 
   return (
     <div className="App">
       <Navbar />
       {/* <Hero /> */}
       <div className='cards'>
-        <Card />
+        {data.map((e,i) => {
+          return (
+            <Card
+              key={i}
+              img={e.img}
+              rating={e.rating}
+              reviewCount={e.reviewCount}
+              country={e.country}
+              title={e.title}
+              price={e.price}
+            />
+          )
+        })}
         {/* <Card/> */}
       </div>
     </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,26 +1,29 @@
 import pic1 from '../assets/images/katie-zaferes.png'
 import star from '../assets/images/star.png'
 
-const Card = () => {
+// const Card = (props) => {
+const Card = (props) => {
+  console.log(props)
   return (
     <section className='card'>
       <div className='img'>
         {/* <div className='img--overlay'> */}
-          <img src={pic1} alt='Katie Zaferes' className='card-img'/>
+          {/* <img src={pic1} alt='Katie Zaferes' className='card-img'/> */}
+          <img src={`../src/assets/images/${props.img}`} alt='Katie Zaferes' className='card-img'/>
           <div>sold out</div>
         {/* </div> */}
       </div>
       <div className='rating'>
         <img src={star} />
-        <p>5.0
-        <span className='gray'>(6) * USA</span>
+        <p>{props.rating}
+        <span className='gray'>({props.reviewCount}) * {props.country}</span>
         </p>
       </div>
       <p className='title'>
-        Life lessons with Katie Zafares
+        {props.title}
       </p>
       <p className='cost-person'>
-        <span className='bold'>From $136</span> / person
+        <span className='bold'>{`From ${props.price}`}</span> / person
       </p>
     </section>
   )
